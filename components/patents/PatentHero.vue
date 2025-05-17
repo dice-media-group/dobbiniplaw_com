011dd0bfb8c0a53d188e4b75096cd5e751725d01
captproton <carl@wdwhub.net>
2025-05-17 15:37:36 -0700

    fix: adjust patent hero button height to match modal close button

--- components/patents/PatentHero.vue
+++ components/patents/PatentHero.vue
@@ -23,11 +23,11 @@
       
       <div class="flex space-x-4">
         <button 
-          class="bg-white text-black px-4 md:px-6 py-2 rounded flex items-center font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          class="bg-white text-black px-4 md:px-6 py-1 rounded flex items-center font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
           @click="$emit('click')"
         >
           <span class="mr-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </span>
